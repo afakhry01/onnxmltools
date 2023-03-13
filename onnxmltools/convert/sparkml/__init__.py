fdca528669fa5bf68e4afa5760871aed109d28aa
afakhry01 <andrew.fakhry@outlook.com>
2023-03-12 21:24:37 -0600

    Add update_registered_converter

--- onnxmltools/convert/sparkml/__init__.py
+++ onnxmltools/convert/sparkml/__init__.py
@@ -2,5 +2,5 @@
 
 from .convert import convert
 from .utils import buildInitialTypesSimple, getTensorTypeFromSpark, buildInputDictSimple
-from .ops_names import get_sparkml_operator_name
+from .ops_names import get_sparkml_operator_name, update_registered_converter
 from .ops_input_output import get_input_names, get_output_names
